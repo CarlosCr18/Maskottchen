4ed8a7e1a1137a0cebbf6eb632bd8aef60b0a0ef
CarlosCr18 <91919845+CarlosCr18@users.noreply.github.com>
2021-12-24 12:38:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const primaryNav = document.querySelector(".header-container");
+const navToggle = document.querySelector(".burgerMenu");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.innerHTML = "&#9776;";
+  } else {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.innerHTML = "🗙";
+  }
+});
+
+function changeData() {
+  primaryNav.setAttribute("data-visible", false);
+  navToggle.innerHTML = "&#9776;";
+}
